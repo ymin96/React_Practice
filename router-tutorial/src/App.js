ad711a615a8c002df3904e612f7c5f7067a7fbf9
Kim YongHwan <ymin96@naver.com>
2020-05-14 17:08:58 +0900

    SPA

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+
+const App = () => {
+    return (
+        <div>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About}/>
+        </div>
+    );
+};
+
