1403e0c930e6a871e3b03785ffebaa2c8c9e349b
Yong Hwan Kim <ymin96@naver.com>
2020-05-15 22:21:28 +0900

    react router

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from './Profile';
 
 const App = () => {
     return (
         <div>
-            <Route path="/" component={Home} />
+            <ul>
+                <li>
+                    <Link to="/">홈</Link>
+                </li>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+                <li>
+                    <Link to="/profile/velopert">velopert 프로필</Link>
+                </li>
+                <li>
+                    <Link to="/profile/gildong">gildong 프로필</Link>
+                </li>
+            </ul>
+            <Route path="/" component={Home} exact={true}/>
             <Route path="/about" component={About}/>
+            <Route path="/profile/:username" component={Profile}/>
         </div>
     );
 };
 
+export default App;
