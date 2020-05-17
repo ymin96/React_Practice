21fdb538a151420731d31ee3e1e5b8f0e6f94d50
Yong Hwan Kim <ymin96@naver.com>
2020-05-18 00:45:04 +0900

    14.7

--- news-viewer/src/components/Categories.js
+++ news-viewer/src/components/Categories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled,{css} from "styled-components";
 
 const categories = [
     {
@@ -58,13 +58,27 @@ const Category = styled.div`
     & + & {
         margin-left: 1rem;
     }
+
+    ${props =>
+        props.active && css`
+            font-weight: 600;
+            border-bottom: 2px solid #22b8cf;
+            color: #22b8cf;
+            &:hover{
+                color: #3bc9db;
+            }
+    `}
 `;
 
-const Categories = () => {
+const Categories = ({onSelect, category}) => {
     return (
         <CategoriesBlock>
             {categories.map((c) => (
-                <Category key={c.name}>{c.text}</Category>
+                <Category 
+                    key={c.name}
+                    active={category === c.name}
+                    onClick={() => onSelect(c.name)}
+                >{c.text}</Category>
             ))}
         </CategoriesBlock>
     );
