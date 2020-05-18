323c9b91a2203588323cf927d1a24c96c75cdf71
Kim YongHwan <ymin96@naver.com>
2020-05-18 15:28:10 +0900

    Finish Context API

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ColorBox from "./components/ColorBox";
+import {ColorProvider} from "./contexts/color";
+import SelectColor from "./components/SelectColor";
+
+const App = () => {
+    return (
+        <ColorProvider>
+            <div>
+                <SelectColor/>
+                <ColorBox />
+            </div>
+        </ColorProvider>
+    );
+};
+
+export default App;
