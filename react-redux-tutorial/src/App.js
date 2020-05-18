f453bd2bd56b1555ed3b2bbb3ee51d68b3f8e914
Kim YongHwan <ymin96@naver.com>
2020-05-18 16:43:15 +0900

    Redux-tutorial

---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CounterContainer from './containers/CounterContainer';
+import Todos from "./components/Todos";
+
+const App = () => {
+    return (
+        <div>
+            <CounterContainer/>
+            <hr/>
+            <Todos/>
+        </div>
+    );
+};
+
+export default App;
---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Counter from "../components/Counter";
+import { connect } from "react-redux";
+
+const CounterContainer = ({ number, increase, decrease }) => {
+    return (
+        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    );
+};
+
+const mapStateToProps = (state) => ({
+    number: state.counter.number,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    increase: () => {
+        console.log("increase");
+    },
+    decrease: () => {
+        console.log("decrease");
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
