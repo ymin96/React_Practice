07b9f13a85dd8da72a4d34f24bbb9a581b7a7342
Kim YongHwan <ymin96@naver.com>
2020-05-21 09:54:25 +0900

    Commit before eject

---
+++ ssr-recipe/src/pages/BluePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Blue from '../components/Blue';
+
+const BluePage = () => {
+    return (
+        <Blue/>
+    );
+};
+
+export default BluePage;
---
+++ ssr-recipe/src/pages/RedPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Red from '../components/Red';
+
+const RedPage = () => {
+    return (
+        <Red/>
+    );
+};
+
+export default RedPage;
