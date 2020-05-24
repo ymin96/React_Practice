74a0dcb79be41e113adab3a7752ddf5e3dc47cfd
Yong Hwan Kim <ymin96@naver.com>
2020-05-25 01:06:33 +0900

    21 finish

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,18 +1,14 @@
 const Router = require('koa-router');
+const postCtrl = require('./posts.ctrl');
+
 const posts = new Router();
 
-const printInfo = (ctx) => {
-    ctx.body = {
-        method: ctx.method,
-        path: ctx.path,
-        params: ctx.params,
-    };
-};
 
-posts.get('/', printInfo);
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
+
+posts.get('/', postCtrl.list);
+posts.post('/', postCtrl.write);
+posts.get('/:id', postCtrl.read);
+posts.delete('/:id', postCtrl.remove);
+posts.put('/:id', postCtrl.replace);
+posts.patch('/:id', postCtrl.update);
 module.exports = posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -68,3 +68,54 @@ exports.remove = (ctx) => {
     posts.splice(index, 1);
     ctx.status = 204; // No Content
 };
+
+/* 포스트 수정(교체)
+PUT /api/posts/:id
+{title, body}
+*/
+exports.replace = (ctx) => {
+    // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용한다.
+    const { id } = ctx.params;
+    // 해당 id를 가진 post가 몇 번째인지 확인한다.
+    const index = posts.findIndex((p) => p.id.toString() === id);
+    // 포스트가 없으면 오류를 반환한다.
+    if (index == -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '포스트가 존재하지 않습니다.',
+        };
+        return;
+    }
+    // 전체 객체를 덮어 씌운다.
+    // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만든다.
+    posts[index] = {
+        id,
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
+
+/* 포스트 수정(특정 필드 변경)
+PATCH /api/posts/:id
+{title, body}
+*/
+exports.update = (ctx) => {
+    // PATCH 메서드는 주어진 필드만 교체한다.
+    const { id } = ctx.params;
+    // 해당 id를 가진 post가 몇 번째인지 확인한다.
+    const index = posts.findIndex((p) => p.id.toString() === id);
+    // 포스트가 없으면 오류를 반환한다.
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: '포스트가 존재하지 않습니다.',
+        };
+        return;
+    }
+    // 기존 값에 정보를 덮어 씌운다.
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
