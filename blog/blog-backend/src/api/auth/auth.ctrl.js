6184d62099d2f09303f578c2a9d5d0436922ad3f
Kim YongHwan <ymin96@naver.com>
2020-05-26 17:02:53 +0900

    23.3.1

---
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,58 @@
+import Joi from '@hapi/joi';
+import User from '../../models/user';
+
+/*
+    POST /api/auth/register
+    {
+        username: 'velopert',
+        password: 'mypass123'
+    }
+*/
+export const register = async (ctx) => {
+    // Request Body 검증하기
+    const schema = Joi.object().keys({
+        username: Joi.string().alphanum().min(3).max(20).required(),
+        password: Joi.string().required(),
+    });
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+    const { username, password } = ctx.request.body;
+    try {
+        // username이 이미 존재하는지 확인
+        const exists = await User.findByUsername(username);
+        if (exists) {
+            ctx.status = 409; // Conflict
+            return;
+        }
+
+        const user = new User({
+            username,
+        });
+        await user.setPassword(password); // 비밀번호 설정
+        await user.save();
+
+        // 응답할 데이터에서 hashedPassword 필드 제거
+        const data = user.toJSON();
+        delete data.hashedPassword;
+        ctx.body = data;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+export const login = async (ctx) => {
+    // 로그인
+};
+
+export const check = async (ctx) => {
+    // 로그인 상태 확인
+};
+
+export const logout = async (ctx) => {
+    // 로그아웃
+};
