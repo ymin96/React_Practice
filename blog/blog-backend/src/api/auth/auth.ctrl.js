a4bb41a94e1a0cf071b8c4337e2e9eac3bd65008
Kim YongHwan <ymin96@naver.com>
2020-05-27 16:32:26 +0900

    23.4.5

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -38,6 +38,12 @@ export const register = async (ctx) => {
 
         // 응답할 데이터에서 hashedPassword 필드 제거
         ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 6024 * 7, // 7일
+            httpOnly: true,
+        });
     } catch (e) {
         ctx.throw(500, e);
     }
@@ -73,15 +79,33 @@ export const login = async (ctx) => {
             return;
         }
         ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 6024 * 7, // 7일
+            httpOnly: true,
+        });
     } catch (e) {
         ctx.throw(500, e);
     }
 };
-
+/*
+    GET /api/auth/check
+*/
 export const check = async (ctx) => {
-    // 로그인 상태 확인
+    const { user } = ctx.state;
+    if (!user) {
+        // 로그인 중 아님
+        ctx.status = 401; // Unauthorized
+        return;
+    }
+    ctx.body = user;
 };
 
+/*
+    POST /api/auth/logout
+*/
 export const logout = async (ctx) => {
-    // 로그아웃
+    ctx.cookies.set('access_token');
+    ctx.status = 204; // No Content
 };
--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
     username: String,
@@ -16,14 +17,29 @@ UserSchema.methods.checkPassword = async function (password) {
     return result; // true | false
 };
 
-UserSchema.methods.serialize = function() {
+UserSchema.methods.serialize = function () {
     const data = this.toJSON();
     delete data.hashedPassword;
     return data;
-}
+};
 
 UserSchema.statics.findByUsername = function (username) {
     return this.findOne({ username });
+};
+
+UserSchema.methods.generateToken = function () {
+    const token = jwt.sign(
+        // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣는다.
+        {
+            _id: this.id,
+            username: this.username,
+        },
+        process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣는다.
+        {
+            expiresIn: '7d', // 7일 동안 유효함
+        },
+    );
+    return token; 
 };
 
 const User = mongoose.model('User', UserSchema);
