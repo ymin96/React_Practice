e8119fc31e1a88ac6af8dfea43249b5634fb5105
Yong Hwan Kim <ymin96@naver.com>
2020-06-02 00:42:55 +0900

    24.3

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -52,12 +52,22 @@ const ButtonWithMarginTop = styled(Button)`
     margin-top: 1rem;
 `;
 
+/**
+ * 에러를 보여 준다
+ */
+const ErrorMessage = styled.div`
+    color:red;
+    text-align: center;
+    font-size:0.875rem;
+    margin-top: 1rem;
+`;
+
 const textMap = {
     login: '로그인',
     register: '회원가입',
 };
 
-const AuthForm = ({ type, form, onChange, onSubmit }) => {
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
     const text = textMap[type];
 
     return (
@@ -89,6 +99,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
                         value={form.passwordConfirm}
                     />
                 )}
+                {error && <ErrorMessage>{error}</ErrorMessage>}
                 <ButtonWithMarginTop cyan fullWidth>
                     {text}
                 </ButtonWithMarginTop>
--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
@@ -7,6 +7,7 @@ import { check } from '../../modules/user';
 import { login } from "../../modules/auth";
 
 const LoginForm = ({ history }) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.login,
@@ -43,6 +44,7 @@ const LoginForm = ({ history }) => {
         if (authError) {
             console.log('오류 발생');
             console.log(authError);
+            setError('로그인 실패')
             return;
         }
         if (auth) {
@@ -63,6 +65,7 @@ const LoginForm = ({ history }) => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
