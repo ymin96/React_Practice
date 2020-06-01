9739759fc994c06b9ffb116e8f824fe1c8f5ae04
Kim YongHwan <ymin96@naver.com>
2020-06-01 17:12:38 +0900

    24.2.6

--- blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
-import { withRouter } from 'react-router-dom';
 import { check } from '../../modules/user';
-import { login } from "../../modules/auth";
+import { withRouter } from 'react-router-dom';
 
-const LoginForm = ({ history }) => {
+const RegisterForm = ({ history }) => {
     const dispatch = useDispatch();
     const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
-        form: auth.login,
+        form: auth.register,
         auth: auth.auth,
         authError: auth.authError,
         user: user.user,
@@ -20,7 +19,7 @@ const LoginForm = ({ history }) => {
         const { value, name } = e.target;
         dispatch(
             changeField({
-                form: 'login',
+                form: 'register',
                 key: name,
                 value,
             }),
@@ -30,15 +29,20 @@ const LoginForm = ({ history }) => {
     // 폼 등록 이벤트 핸들러
     const onSubmit = (e) => {
         e.preventDefault();
-        const { username, password } = form;
-        dispatch(login({ username, password }));
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm) {
+            // TODO: 오류 처리
+            return;
+        }
+        dispatch(register({ username, password }));
     };
 
     // 컴포넌트가 처음 렌더링될 때 form 을 초기화함
     useEffect(() => {
-        dispatch(initializeForm('login'));
+        dispatch(initializeForm('register'));
     }, [dispatch]);
 
+    // 회원가입 성공/실패 처리
     useEffect(() => {
         if (authError) {
             console.log('오류 발생');
@@ -46,20 +50,22 @@ const LoginForm = ({ history }) => {
             return;
         }
         if (auth) {
-            console.log('로그인 성공');
+            console.log('회원가입 성공');
+            console.log(auth);
             dispatch(check());
         }
     }, [auth, authError, dispatch]);
 
+    // user 값이 잘 설정되었는지 확인
     useEffect(() => {
         if (user) {
             history.push('/');
         }
-    }, [history, user]);
+    }, [user, history]);
 
     return (
         <AuthForm
-            type="login"
+            type="register"
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
@@ -67,4 +73,4 @@ const LoginForm = ({ history }) => {
     );
 };
 
-export default withRouter(LoginForm);
+export default withRouter(RegisterForm);
--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './modules';
+import rootReducer,{rootSaga} from './modules';
+import createSagaMiddleware from "redux-saga";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
