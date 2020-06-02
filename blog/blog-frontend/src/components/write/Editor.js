ec34e6a4ec69190f83ad52bb407916214731fd77
Kim YongHwan <ymin96@naver.com>
2020-06-02 17:07:30 +0900

    25.1

---
+++ blog/blog-frontend/src/components/write/Editor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Quill from "quill";
+import 'quill/dist/quill.bubbls.css';
+import styled from "styled-components";
+import palette from "../../lib/styles/palette";
+import Responsive from "../common/Responsive";
+
+const EditorBlock = styled(Responsive)`
+
+`;
+
+const Editor = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Editor;
