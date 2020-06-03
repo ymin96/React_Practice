422ce09cace5237cd8a82c0ea6c198f7eaa78f90
Kim YongHwan <ymin96@naver.com>
2020-06-03 17:12:17 +0900

    25.3.3

--- blog/blog-frontend/src/components/write/TagBox.js
+++ blog/blog-frontend/src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -32,7 +32,7 @@ const TagForm = styled.form`
     input {
         padding: 0.5rem;
         flex: 1;
-        min-width:0;
+        min-width: 0;
     }
 
     button {
@@ -64,26 +64,73 @@ const TagListBlock = styled.div`
 `;
 
 // React.memo 를 사용하여 tag 값이 바뀔 때만 리렌더링되도록 처리
-const TagItem = React.memo(({ tag }) => <Tag>#{tag}</Tag>);
+const TagItem = React.memo(({ tag, onRemove }) => (
+    <Tag onClick={() => onRemove(tag)}>#{tag}</Tag>
+));
 
 // React.memo 를 사용하여 tags 값이 바뀔 때만 리렌더링되도록 처리
-const TagList = React.memo(({ tags }) => (
+const TagList = React.memo(({ tags, onRemove }) => (
     <TagListBlock>
         {tags.map((tag) => (
-            <TagItem key={tag} tag={tag} />
+            <TagItem key={tag} tag={tag} onRemove={onRemove} />
         ))}
     </TagListBlock>
 ));
 
-const TagBox = () => {
+const TagBox = ({ tags, onChangeTags }) => {
+    const [input, setInput] = useState('');
+    const [localTags, setLocalTags] = useState([]);
+
+    const insertTag = useCallback(
+        (tag) => {
+            if (!tag) return; // 공백이라면 추가하지 않음
+            if (localTags.includes(tag)) return; // 이미 존재한다면 추가하지 않음
+            const nextTags = [...localTags, tag];
+            setLocalTags([...localTags, tag]);
+            onChangeTags(nextTags);
+        },
+        [localTags, onChangeTags],
+    );
+
+    const onRemove = useCallback(
+        (tag) => {
+            const nextTags = localTags.filter((t) => t !== tag);
+            setLocalTags(nextTags);
+            onChangeTags(nextTags);
+        },
+        [localTags, onChangeTags],
+    );
+
+    const onChange = useCallback((e) => {
+        setInput(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        (e) => {
+            e.preventDefault();
+            insertTag(input.trim()); // 앞뒤 공백을 없앤 후 등록
+            setInput(''); // input 초기화
+        },
+        [input, insertTag],
+    );
+
+    // tags 값이 바뀔 때
+    useEffect(() => {
+        setLocalTags(tags);
+    }, [tags]);
+
     return (
         <TagBoxBlock>
             <h4>태그</h4>
-            <TagForm>
-                <input placeholder="태그를 입력하세요" />
+            <TagForm onSubmit={onSubmit}>
+                <input
+                    placeholder="태그를 입력하세요"
+                    value={input}
+                    onChange={onChange}
+                />
                 <button type="submit">추가</button>
             </TagForm>
-            <TagList tags={['태그1', '태그2', '태그3']} />
+            <TagList tags={localTags} onRemove={onRemove} />
         </TagBoxBlock>
     );
 };
--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
-import Editor from '../components/write/Editor';
-import TagBox from '../components/write/TagBox';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor/>
-            <TagBox/>
+            <EditorContainer/>
+            <TagBoxContainer/>
+            <WriteActionButtonsContainer/>
         </Responsive>
     );
 };
