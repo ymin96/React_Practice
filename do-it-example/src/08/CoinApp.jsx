1395aa8e313ba58911de736e924957af132cc8c3
Kim YongHwan <ymin96@naver.com>
2020-08-20 16:48:25 +0900

    p.431

---
+++ do-it-example/src/08/CoinApp.jsx
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import AppLayout from "./components/AppLayout";
+
+class CoinApp extends PureComponent {
+    render() {
+        return <AppLayout>본문</AppLayout>;
+    }
+}
+
+export default CoinApp;
